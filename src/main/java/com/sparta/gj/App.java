a0230701629c1649a811f6710b2ed548e9695481
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-17 14:31:05 +0100

    add rest

--- src/main/java/com/sparta/gj/App.java
+++ src/main/java/com/sparta/gj/App.java
@@ -1,13 +1,16 @@
 package com.sparta.gj;
 
+import org.json.simple.JSONObject;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        JSONObject userDetails = new JSONObject();
+        System.out.println(userDetails.getClass());
     }
 }
